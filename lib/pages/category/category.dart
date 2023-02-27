7520a61b115cc64b536ff88181f91ddc81cbbc47
valentioaditama <valentioaditama8@gmail.com>
2023-02-27 13:02:27 +0700

    update

--- lib/pages/category/category.dart
+++ lib/pages/category/category.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class category extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Category"),
-      ),
-      body: Center(
-        child: Text("category"),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Scaffold(
+        body: SingleChildScrollView( 
+          child: Padding(
+            padding: EdgeInsets.only(top: 10)
+            ),
+        ),
       ),
     );
   }

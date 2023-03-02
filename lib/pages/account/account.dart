e18d01cba6774c1909142baec6fc6aa1c34caf5f
valentioaditama <valentioaditama8@gmail.com>
2023-03-02 09:07:27 +0700

    Update account

--- lib/pages/account/account.dart
+++ lib/pages/account/account.dart
@@ -6,7 +6,28 @@ class account extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
-        body: ,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hi, User!',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  'Your are not logged in',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat', fontWeight: FontWeight.w200),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
